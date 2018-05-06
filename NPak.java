00d0c7d992fcb416c8a3f8f303648683fb90aaba
pvinodp <pvinod.mit@gmail.com>
2018-05-06 23:08:04 +0530

    Create NPak.java

---
+++ NPak.java
@@ -0,0 +1,8 @@
+public class Book {
+    private final String name;
+    private final double price;
+    public Star(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }    
+ }
